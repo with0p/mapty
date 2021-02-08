3b6939db129e3e40f3fbce6ff53edbc067279a00
with0p <with2p@yandex.ru>
2021-02-08 18:44:51 +0300

    Workout, Running, Cycling classes

--- script.js
+++ script.js
@@ -11,6 +11,44 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = Math.trunc(this.date.getTime() * Math.random());
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat,lng]
+    this.distance = distance; //km
+    this.duration = duration; // min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevation) {
+    super(coords, distance, duration);
+    this.elevation = elevation;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run = new Running([10, 10], 6, 7, 8);
+const cyc = new Cycling([10, 10], 6, 7, 8);
+console.log(run, cyc);
+
 class App {
   #map;
   #mapEvent;
