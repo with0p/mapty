b865db50c1fa8d06c397628103d6a7f1bc5fede2
with0p <with2p@yandex.ru>
2021-02-08 14:40:00 +0300

    User's current location

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, 12);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords).addTo(map).bindPopup('Here you are').openPopup();
+  },
+  function () {
+    alert('No map then,sorry');
+  }
+);
